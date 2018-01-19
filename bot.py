1f5eb2d3c112c63edc1ed2c64af09a4225318810
reinhart <reinhart@YRs-Mac.local>
2018-01-19 05:20:19 +0300

    add var from env

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+import os
 import telebot
+import requests
 
-bot = telebot.TeleBot('BOT_TOKEN')
+bot = telebot.TeleBot(os.environ.get('BOT_TOKEN'))
+
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
